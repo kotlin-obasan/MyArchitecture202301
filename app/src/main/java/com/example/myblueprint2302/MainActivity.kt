f286569824605d6d2fc20dcb841b5194db507181
KotlinDaisuki <yuuitakeda@gmail.com>
2023-02-26 19:39:20 +0900

    NavigationComponentを追加

--- app/src/main/java/com/example/myblueprint2302/MainActivity.kt
+++ app/src/main/java/com/example/myblueprint2302/MainActivity.kt
@@ -2,19 +2,29 @@ package com.example.myblueprint2302
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleObserver
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.myblueprint2302.databinding.ActivityMainBinding
 import com.example.myblueprint2302.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleObserver {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //ナビゲーションの設定
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
